afd6d31c18b8f8af1e69c0c991a9382f98a4c17f
Sethulakshmi <sethulakshmi.pm@razorthink.com>
2018-07-16 16:38:35 +0530

    Animation

--- assignment3/src/component/ReduxPage.js
+++ assignment3/src/component/ReduxPage.js
@@ -3,36 +3,36 @@ import React, { Component } from 'react';
 import '../styles.css';
 
 class ReduxPage extends Component {
-  setIntervalFunc = (func) => {
-    this.interval = setInterval(func, 500);
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      cards: {
-        index: -1,
-      },
-    };
-  }
-
-  componentDidMount() {
-    this.setIntervalFunc(() => {
-      if (this.state.cards.index === this.props.dataSet.length) {
-        this.componentWillUnmount();
-        return;
-      }
-      this.setState({
-        cards: {
-          index: this.state.cards.index + 1,
-        },
-      });
-    });
-  }
-
-  componentWillUnmount() {
-    if (this.interval) clearInterval(this.interval);
-  }
+  // setIntervalFunc = (func) => {
+  //   this.interval = setInterval(func, 1500);
+  // };
+  //
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     cards: {
+  //       index: -1,
+  //     },
+  //   };
+  // }
+  //
+  // componentDidMount() {
+  //   this.setIntervalFunc(() => {
+  //     if (this.state.cards.index === this.props.dataSet.length) {
+  //       this.componentWillUnmount();
+  //       return;
+  //     }
+  //     this.setState({
+  //       cards: {
+  //         index: this.state.cards.index + 1,
+  //       },
+  //     });
+  //   });
+  // }
+  //
+  // componentWillUnmount() {
+  //   if (this.interval) clearInterval(this.interval);
+  // }
 
   render() {
 
@@ -41,37 +41,45 @@ class ReduxPage extends Component {
         <h2 className="welcome">
           Data
         </h2>
-        {/*<button*/}
-          {/*onClick={this.props.getData}*/}
-        {/*>*/}
-          {/*ClickHere*/}
-        {/*</button>*/}
 
-        <div className={"cardWrapper"}>
-          {this.props.dataSet &&
-          this.props.dataSet.map((item, i) =>
+        {/*<div className={"cardWrapper"}>*/}
+        {/*{this.props.dataSet &&*/}
+        {/*this.props.dataSet.map((item, i) =>*/}
+        {/*<div*/}
+        {/*key={i}*/}
+        {/*className={`headingText*/}
+        {/*${(this.state.cards.index === i) ? 'currentCard' : ''}*/}
+        {/*`}*/}
+        {/*onClick={() => {*/}
+        {/*this.setState({*/}
+        {/*cards:{*/}
+        {/*index: i,*/}
+        {/*}*/}
+        {/*});*/}
+        {/*}}*/}
+        {/*>*/}
+        {/*<span*/}
+        {/*className={'textStyle'}*/}
+        {/*>*/}
+        {/*{item}*/}
+        {/*</span>*/}
+        {/*</div>,*/}
+        {/*)}*/}
+        {/*</div>*/}
+
+        <div className="dataWrapper">
+          {this.props.dataSet.map((item, i) =>
             <div
-              key={i}
-              className={`headingText
-              ${(this.state.cards.index === i) ? 'currentCard' : ''}
-              ${(this.state.cards.index - 1 === i) ? 'previousCard' : ''}
-              `}
-              onClick={() => {
-                this.setState({
-                  cards:{
-                    index: i,
-                  }
-                });
+              className={'child'}
+              style={{
+                animationDelay: `${0.5 * i}s`
               }}
             >
-              <span
-                className={'textStyle'}
-              >
-                {item}
-              </span>
-            </div>,
+              {item}
+            </div>
           )}
         </div>
+
       </div>
     );
   }
